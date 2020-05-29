e9a42910d16e23c83d8aca65cd79560bfa82e754
Abd3lrhm4n <abdelrhmanelsaid681@gmail.com>
2020-05-29 17:19:27 +0200

    solved levelwidth

--- levelwidth/index.js
+++ levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let counters = [0]
+    let arr = [root, 's']
+
+    while(arr.length > 1) {
+        const node = arr.shift();
+
+        if(node === 's') {
+            counters.push(0)
+            arr.push('s')
+        } else {
+            arr.push(...node.children)
+            counters[counters.length - 1]++
+        }
+
+    }
+    return counters;
+}
 
 module.exports = levelWidth;
